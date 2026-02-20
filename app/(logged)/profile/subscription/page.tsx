38ae32c1bdf2d481a7f329bf7854de154b050efe
MalicknND <ndiayemalick062@gmail.com>
2026-02-20 10:56:12 +0100

    Remplacez l'erreur de configuration de Stripe par une redirection vers la page 404

--- app/(logged)/profile/subscription/page.tsx
+++ app/(logged)/profile/subscription/page.tsx
@@ -1,14 +1,15 @@
 'use client'
 
+import { notFound } from "next/navigation"
 import { projectConfig } from "@/config/project"
 import { useSubscription } from "@/lib/hooks/use-subscription"
 import { CurrentSubscription } from "./_components/current-subscription"
 import { SubscriptionPlans } from "./_components/subscription-plans"
 
 export default function SubscriptionPage() {
-  const subscription = useSubscription((state) => state.subscription)  
+  const subscription = useSubscription((state) => state.subscription)
   if (!projectConfig.features.stripe.enabled) {
-    throw new Error("Stripe is not enabled in project config")
+    notFound()
   }
   
   return (
